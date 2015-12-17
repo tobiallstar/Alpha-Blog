d1f1f26df28845ff2e63fe7a9b6242423a8ed6a6
Tobi Popoola <popoolatobi@gmail.com>
2015-12-16 23:15:11 -0500

    Create articles table, model and validations

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ActiveRecord::Base
+  validates :title, presence: true, length: { mininum: 3, maximum: 50}
+  validates :description, presence: true, length: { mininum: 10, maximum: 300}
+end
