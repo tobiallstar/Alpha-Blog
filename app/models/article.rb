b0cf719503e0b448d9a17838e54b7808b1bb4b2e
Tobi Popoola <popoolatobi@gmail.com>
2015-12-18 23:07:13 -0500

    Added some names

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: { mininum: 3, maximum: 50}
-  validates :description, presence: true, length: { mininum: 10, maximum: 300 }
+  validates :title, presence: true, length: { in: 3...50}
+  validates :description, presence: true, length: { in: 10..300}
 end
