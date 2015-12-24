bdd0d76662e94190db4766ce71c5923c085f00f3
Tobi Popoola <popoolatobi@gmail.com>
2015-12-24 18:56:23 -0500

    Check something

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,40 +25,32 @@ class ArticlesController < ApplicationController
   # POST /articles.json
   def create
     @article = Article.new(article_params)
-    @article.save
-    respond_to do |format|
       if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
+        flash[:success] = 'Article was successfully created.' 
+        redirect_to article_path(@article)
       else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+        render 'new'
     end
   end
 
   # PATCH/PUT /articles/1
   # PATCH/PUT /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+        flash[:success] = 'Article was successfully updated.' 
+        redirect_to article_path(@article)
       else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+        render 'edit'
     end
   end
 
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
-    @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+     @article.destroy
+        flash[:danger] = 'Article was successfully deleted.' 
+        redirect_to article_path
   end
 
   private
