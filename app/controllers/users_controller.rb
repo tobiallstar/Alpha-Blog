dfd55c8ae402ecf83b8bbcca3780bd06c7d07d99
Tobi Popoola <popoolatobi@gmail.com>
2015-12-25 20:47:22 -0500

    Signup users from UI

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,14 +26,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+         flash[:success] = "Welcome to EphNow, #{@user.username}."
+         redirect_to articles_path
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+        render 'new'
     end
   end
 
@@ -69,6 +66,7 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :email)
+      params.require(:user).permit(:username, :email, :password)
     end
+
 end
