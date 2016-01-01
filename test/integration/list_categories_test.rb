212856781fd7155a62f5543dd560cb29b8947f8e
Tobi Popoola <popoolatobi@gmail.com>
2016-01-01 01:48:14 -0500

    Fix list categories test bug

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
   def setup
-    @category = Category.create(name: "academics")
+    @category = Category.create(name: "sports")
     @category2 = Category.create(name: "relationships")
   end
 
