c34351ef33648870a8b07741ebd0febedd28e5e6
Tobi Popoola <popoolatobi@gmail.com>
2016-01-14 23:59:12 -0500

    More stuff

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ActiveRecord::Base
   has_many :article_categories
   has_many :categories, through: :article_categories
   validates :title, presence: true, length: { in: 3...50}
-  validates :description, presence: true, length: { in: 10..300}
+  validates :description, presence: true, length: { in: 10..1000}
   validates :user_id, presence: true
 end
