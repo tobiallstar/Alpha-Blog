d1876d12381e45a8018532f651f7398149751bbe
Tobi Popoola <popoolatobi@gmail.com>
2016-01-18 13:12:26 -0500

    Added user?

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+User.create(username: "BoyAtWilliams", email: "omp1@williams.edu", password: "Omega100", admin: "true")
+User.create(username: "Elephant22", email: "ap9@williams.edu", password: "Ephnow1234", admin: "true")
+User.create(username: "cubes", email: "ggg2@williams.edu", password: "22Bluebell!", admin: "true")
+User.create(username: "aliaR", email: "afr1@williams.edu", password: "purrthecat1", admin: "true")
+User.create(username: "jea1", email: "jea11@williams.edu", password: "#Monkey45boy", admin: "true")
