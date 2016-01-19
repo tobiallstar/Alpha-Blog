362ed0389da51657141991c7c3acabb63eec9c9a
Tobi Popoola <popoolatobi@gmail.com>
2016-01-18 20:57:09 -0500

    Added favicon, fixed admin users, now about to launch finallllllly :):):)

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,4 @@ User.create(username: "BoyAtWilliams", email: "omp1@williams.edu", password: "Om
 User.create(username: "Elephant22", email: "ap9@williams.edu", password: "Ephnow1234", admin: "true")
 User.create(username: "cubes", email: "ggg2@williams.edu", password: "22Bluebell!", admin: "true")
 User.create(username: "aliaR", email: "afr1@williams.edu", password: "purrthecat1", admin: "true")
-User.create(username: "jea1", email: "jea11@williams.edu", password: "#Monkey45boy", admin: "true")
+User.create(username: "jea1", email: "jea1@williams.edu", password: "#Monkey45boy", admin: "true")
